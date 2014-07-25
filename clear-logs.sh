ca43891dc1bd162ce1a4ff4a0db1be6bbedb5114
ggkitsas <ggkitsas@yahoo.com>
2014-07-25 10:27:48 +0300

    log helper scripts

---
+++ clear-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ~/.ccnet/logs
+rm ccnet.log
+touch ccnet.log
+
+rm applet.log
+touch applet.log
+
+rm seafile.log
+touch seafile.log
+
---
+++ print-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~/.ccnet/logs
+
+echo "***********************APPLET LOG*****************************"
+cat applet.log 
+echo "***********************CCNET LOG*****************************"
+cat ccnet.log
+echo "***********************SEAFILE LOG*****************************"
+cat seafile.log
