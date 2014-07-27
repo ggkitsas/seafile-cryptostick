91352ebd3082ac6661a7b829c682873ed446d371
ggkitsas <ggkitsas@yahoo.com>
2014-07-27 09:50:13 +0300

    and clear the db's

--- clear-fs.sh
+++ clear-fs.sh
@@ -7,3 +7,10 @@ cd ~/Seafile/Seafile/.seafile-data/storage
 cd blocks/ && rm -rf * && cd -
 cd commits/ && rm -rf * && cd -
 cd fs/ && rm -rf *
+
+cd ~/Seafile/Seafile/.seafile-data
+sqlite3 repo.db "delete from Repo where 1=1;"
+sqlite3 repo.db "delete from RepoBranch where 1=1;"
+sqlite3 repo.db "delete from RepoKeys where 1=1;"
+sqlite3 repo.db "delete from RepoProperty where 1=1;"
+sqlite3 branch.db "delete from Branch where 1=1;"
