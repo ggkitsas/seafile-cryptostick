7a2a3408d1408ad0fe1a5e1bd017811ababad235
ggkitsas <ggkitsas@yahoo.com>
2014-11-14 14:22:12 +0100

    print logs options

--- print-logs.sh
+++ print-logs.sh
@@ -2,9 +2,22 @@
 
 cd ~/.ccnet/logs
 
-echo "***********************APPLET LOG*****************************"
-cat applet.log 
-echo "***********************CCNET LOG*****************************"
-cat ccnet.log
-echo "***********************SEAFILE LOG*****************************"
-cat seafile.log
+if [ $1 = "ccnet" ]; then
+    echo "***********************CCNET LOG*****************************"
+    cat ccnet.log
+elif [ $1 = "applet" ]; then
+    echo "***********************APPLET LOG*****************************"
+    cat applet.log 
+elif [ $1 = "seafile" ]; then
+    echo "***********************SEAFILE LOG*****************************"
+    cat seafile.log
+else
+    echo "***********************CCNET LOG*****************************"
+    cat ccnet.log
+    echo "***********************SEAFILE LOG*****************************"
+    cat seafile.log
+    echo "***********************APPLET LOG*****************************"
+    cat applet.log 
+fi
+
+
